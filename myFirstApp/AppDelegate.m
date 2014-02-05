cf7fbadc54995e6d1dcf2e34b8ff5f4dd4d3a61d
Mayank Sinha <mayank.sinha022@gmail.com>
2014-02-05 15:02:45 -0500

    First commit with View controller and marker on map.

--- myFirstApp/AppDelegate.m
+++ myFirstApp/AppDelegate.m
@@ -7,12 +7,14 @@
 //
 
 #import "AppDelegate.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [GMSServices provideAPIKey:@"AIzaSyCzkapJIEETCiDSNrWlU6PVPdA2tv5Q7hY"];
     return YES;
 }
 							
